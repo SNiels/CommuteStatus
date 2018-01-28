e234d98e759b576abc791af7f17437cb3e17835a
Niels Swimberghe <nielsswimberghe@gmail.com>
2018-01-28 01:17:52 -0500

    add app insights

--- CommuteStatus/src/app.ts
+++ CommuteStatus/src/app.ts
@@ -1,9 +1,12 @@
-﻿import * as debug from 'debug';
-import * as express from 'express';
+﻿import * as express from 'express';
 import * as path from 'path';
 import * as cookieParser from 'cookie-parser';
 import * as bodyParser from 'body-parser';
+import * as appInsights from "applicationinsights";
+import {appInsightsKey} from 'src/options';
 import googleActions from 'src/routes/googleActions';
+appInsights.setup(appInsightsKey);
+appInsights.start();
 
 var app = express();
 app.use(bodyParser.json());
@@ -15,5 +18,5 @@ app.use('/', googleActions);
 app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(app.get('port'), function () {
-    debug('Express server listening on port ' + server.address().port);
+    console.log('Express server listening on port ' + server.address().port);
 });
